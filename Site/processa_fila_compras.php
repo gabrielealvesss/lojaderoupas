2073c5cbe776f00fed3d208d3a1dec3ca068e1ce
gabrielealvesss <138074861+gabrielealvesss@users.noreply.github.com>
2023-06-30 13:40:22 -0300

    Update processa_fila_compras.php

--- Site/processa_fila_compras.php
+++ Site/processa_fila_compras.php
@@ -1,18 +1,18 @@
 <?php	
-	$conectar = mysqli_connect ("localhost", "root", "", "sistema_modelo");
+	$conectar = mysqli_connect ("localhost", "root", "", "lojaderoupas");
 				
 	
 	$cod = $_GET["codigo"];	
 	
-	$sql_altera = "UPDATE amplificadores 		
-				   SET 		fila_compra_amp = 'S'
-				   WHERE 	cod_amp = '$cod'";
+	$sql_altera = "UPDATE roupa 		
+				   SET 		fila_compra_roupa = 'S'
+				   WHERE 	cod_roupa = '$cod'";
 	$sql_resultado_alteracao = mysqli_query ($conectar, $sql_altera);
 
 	if ($sql_resultado_alteracao == true)
 	{
 		echo "<script>
-				alert ('Amplificador colocado na fila de compra com sucesso') 
+				alert ('Roupa colocado na fila de compra com sucesso') 
 			  </script>";
 		echo "<script> 
 				 location.href = ('vendas.php') 
@@ -22,7 +22,7 @@
 	else
 	{    
 		echo "<script> 
-				alert ('Ocorreu um erro no servidor. O amplificador não foi colocado na fila de compras. Tente de novo') 
+				alert ('Ocorreu um erro no servidor. A roupa nï¿½o foi colocado na fila de compras. Tente de novo') 
 			</script>";
 		echo "<script> 
 				location.href ('vendas.php') 
